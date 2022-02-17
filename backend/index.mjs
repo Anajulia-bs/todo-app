883d490182105a39cc17e126fbd3b3888d6aff0d
Anajulia <juliehindius@gmail.com>
2022-02-17 10:59:38 -0300

    cria endpoints

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,9 +1,29 @@
-import cors from 'cors';
-import express from 'express';
-import dotenv from 'dotenv';
-
-const app = express()
+//@ts-check
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import {
+  postTodo,
+  deleteTodo,
+  putTodo,
+  getTodos,
+} from "./src/controller/todoController.mjs";
+const app = express();
+const PORT = 3000;
 
 dotenv.config();
 
 app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.status(200).send("its working"));
+
+app.post("/todo", postTodo);
+app.get("/todos", getTodos);
+app.delete("/todo/:id", deleteTodo);
+app.put("/todo/", putTodo);
+
+app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
+
+export default app;
