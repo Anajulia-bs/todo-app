45d498ec7b086b77bb9cc36eb523049d7ad09b9b
Anajulia <juliehindius@gmail.com>
2022-02-17 10:58:02 -0300

    cria service -  cria regra de negocio

--- backend/src/service/todoService.mjs
+++ backend/src/service/todoService.mjs
@@ -1,5 +1,28 @@
-import * as todoModel from ('../model/todoModel.mjs');
+//@ts-check
+import * as todoModel from "../model/todoModel.mjs";
 
-export function createTodo ({todo}) {
-    todoModel.addTodoDB({todo})
+export function createTodo(todo) {
+  return todoModel.addTodoDB({
+    ...todo,
+    createdAt: Date.now(),
+    status: "pending",
+  });
+}
+
+/**
+ * @param {{ _id: any; }} todo
+ */
+export function deleteTodo(todo) {
+  return todoModel.deleteTodoDB(todo);
+}
+
+/**
+ * @param {any} id
+ * @param {{ name: any; }} newTodo
+ */
+export function updateTodo(id, newTodo) {
+  return todoModel.updateTodoDB({ id, newTodo });
+}
+export function listTodos() {
+  return todoModel.getTodosDB();
 }
