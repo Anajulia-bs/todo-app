4e0313e83976678c5ed8cd6672fc2f4dcb747d5c
Anajulia <juliehindius@gmail.com>
2022-02-17 10:53:06 -0300

    cria model - lida com o BD

--- backend/src/model/todoModel.mjs
+++ backend/src/model/todoModel.mjs
@@ -1,19 +1,34 @@
-import connections from ('../connection');
-
+//@ts-check
+import { ObjectId } from "mongodb";
+import connections from "../connection.js";
 
 // choose the collection
 const getCollection = () =>
-  connections().then((db) => db.collection('todoList'));
+  connections().then((db) => db.collection("todoList"));
 
 // Add to-do item on DB
-export const addTodoDB = async ({ todo }) =>
-  (await getCollection())
-    .insertOne({ todo })
-    .then(({ insertedId }) => ({
-      _id: ObjectId(insertedId),
-     todo,
-    }));
+export const addTodoDB = async (todo) =>
+  (await getCollection()).insertOne(todo).then(({ insertedId }) => ({
+    id: insertedId,
+    todo,
+  }));
 
 // Delete to-do by ID in DB
-export const deleteTodoDB = async ({ id }) =>
-(await getCollection()).deleteOne({_id:id});
+export const deleteTodoDB = async ({ _id }) =>
+  // Using ObjectId in order to properly find the document to be deleted
+  (await getCollection()).deleteOne({ _id: new ObjectId(_id) });
+
+// Update to-do by ID in DB
+export const updateTodoDB = async ({ id, newTodo }) => {
+  console.log({ id, newTodo });
+  return (await getCollection()).findOneAndUpdate({ _id: new ObjectId(id) }, [
+    { $set: newTodo },
+  ]);
+};
+
+export const getTodosDB = async () =>
+  (await (await getCollection()).find().toArray()).map((todo) => ({
+    id: todo._id,
+    name: todo.name,
+    status: todo.status,
+  }));
